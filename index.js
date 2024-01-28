eca6e63537a8ba186465fbe2240e4e42223089b8
Adriana Sun <adriana.sun.07@gmail.com>
2024-01-28 10:06:50 -0600

    Added js

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ submit.addEventListener("click", ()=>{
     file = imageField.files[0];
     sessionStorage.setItem("userImage", URL.createObjectURL(file));
 });
+
+submit.addEventListener("click", (e)=> {
+    // adding an event listener to each "submit" button
+    // that checks if the associated form has all required fields filled out
+    
+    let allAreFilled = true;    // assume fields are filled out by default
+    button.parentElement.querySelectorAll("[required]").forEach(function(i) {
+    if(!allAreFilled) return;   // if one of the prev required fields is empty, just return so as to not waste time
+    
+    if(!i.value) 
+        allAreFilled = false;  // if there is no value, then the required field is not filled
+    
+    if(i.tagName.toLowerCase() === "select"){
+        if(i.value === "select") 
+            allAreFilled = false;
+    }
+    });
+    
+    if(!allAreFilled){
+        setInterval(() => button.classList.add("invalidSubmitAnimation"), 200);
+        button.classList.remove("invalidSubmitAnimation");
+        return;
+    }
+
+    // if user is making a reservation, then give them a reservation ID after they complete it
+    if(button.parentElement.className === "inputForm"){
+        var resIdNotif = document.querySelector(".resIdNotif");
+        var resId = resIdNotif.querySelector("span");
+        resId.innerText = generateID();
+        resIdNotif.style.display = "block";
+    }
+
+    alertElement.style.opacity = '1';
+});
