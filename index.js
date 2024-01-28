22ae6d265646b1181a562ba7aa73645b877ebd94
Adriana Sun <adriana.sun.07@gmail.com>
2024-01-28 10:35:24 -0600

    Commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const submit = document.querySelector("input[type=submit]");
+const submit = document.querySelector(".submit");
 const nameField = document.querySelector("#name");
 const imageField = document.querySelector('input[type=file]');
 const image = document.querySelector("img");
@@ -7,14 +7,12 @@ submit.addEventListener("click", ()=>{
     sessionStorage.setItem("userName", nameField.value);
     file = imageField.files[0];
     sessionStorage.setItem("userImage", URL.createObjectURL(file));
-});
 
-submit.addEventListener("click", (e)=> {
     // adding an event listener to each "submit" button
     // that checks if the associated form has all required fields filled out
     
     let allAreFilled = true;    // assume fields are filled out by default
-    button.parentElement.querySelectorAll("[required]").forEach(function(i) {
+    submit.parentElement.querySelectorAll("[required]").forEach(function(i) {
     if(!allAreFilled) return;   // if one of the prev required fields is empty, just return so as to not waste time
     
     if(!i.value) 
@@ -27,18 +25,8 @@ submit.addEventListener("click", (e)=> {
     });
     
     if(!allAreFilled){
-        setInterval(() => button.classList.add("invalidSubmitAnimation"), 200);
-        button.classList.remove("invalidSubmitAnimation");
         return;
     }
-
-    // if user is making a reservation, then give them a reservation ID after they complete it
-    if(button.parentElement.className === "inputForm"){
-        var resIdNotif = document.querySelector(".resIdNotif");
-        var resId = resIdNotif.querySelector("span");
-        resId.innerText = generateID();
-        resIdNotif.style.display = "block";
-    }
-
-    alertElement.style.opacity = '1';
+    
+    submit.setAttribute('href', "maps.html");
 });
