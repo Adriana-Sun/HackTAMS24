1b0e3ec75bb7d856e3fb1ad11a691e595bc26373
Cathy Du <cat@dynamic-10-125-240-122.dynamic.unt.edu>
2024-01-27 22:49:35 -0600

    started button changing

---
+++ fight.js
@@ -0,0 +1,21 @@
+function clickAction(){
+    document.getElementById("choice1").innerHTML="Pickup Line";
+    document.getElementById("choice2").innerHTML="Message";
+    document.getElementById("choice3").innerHTML="Ice Breakers";
+    document.getElementById("choice4").innerHTML="Beg";
+}
+
+function clickItems(){
+    document.getElementById("choice1").innerHTML="Gift";
+    document.getElementById("choice2").innerHTML="Berry";
+    document.getElementById("choice3").innerHTML="Rare Candy";
+    document.getElementById("choice4").innerHTML="Bulbasaur"; 
+}
+
+function clickInfo(){
+    //show the card
+}
+
+function clickRun(){
+    //return to card page
+}
